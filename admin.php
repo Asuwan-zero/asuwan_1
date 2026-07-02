0915c54cc724e6c80c121757a859be753352e72d
wangdev123 <zerotwowann@gmail.com>
2026-07-02 11:47:53 +0700

    อัพเดทส่วนแอดมิน

--- admin.php
+++ admin.php
@@ -1,3 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "coffe";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname,3307);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
 <!DOCTYPE html>
 <html lang="th">
 <head>
